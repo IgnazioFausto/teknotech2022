e20795cb03830f99f4fb3d5cfbd5180946ccfc36
IgnazioFausto <nachojam777@gmail.com>
2022-01-23 21:16:16 -0300

    ordenado

---
+++ js/cardsHome.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('button.open').click(function () {
+      $('.box-content').addClass('activate');
+    });
+    $('a.close').click(function () {
+      $('.box-content').removeClass('activate');
+    });
+  });
+  $(document).ready(function () {
+    $('button.open2').click(function () {
+      $('.box-content2').addClass('activate');
+    });
+    $('a.close2').click(function () {
+      $('.box-content2').removeClass('activate');
+    });
+  });
+  $(document).ready(function () {
+    $('button.open3').click(function () {
+      $('.box-content3').addClass('activate');
+    });
+    $('a.close3').click(function () {
+      $('.box-content3').removeClass('activate');
+    });
+  });
